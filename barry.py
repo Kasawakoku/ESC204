4894a6b18cfb36f915b2ea3f50d330a2074ef350
Bruce <qiub0222@yahoo.com>
2026-03-12 15:46:10 -0400

    for testing temp sensor with motor

--- barry.py
+++ barry.py
@@ -6,8 +6,8 @@ import adafruit_am2320
 
 # ---------------- CONFIGURATION ---------------- #
 HOT_TEMP_THRESHOLD = 25.5  # Celsius threshold to trigger hot mode 
-COLD_TEMP_THRESHOLD = 23  # Celsius threshold to trigger cold mode
-# need to look back on Joburg temperature. Ideally switch should be daily
+COLD_TEMP_THRESHOLD = 25  # Celsius threshold to trigger cold mode
+# for testing. need to look back on Joburg temperature. Ideally switch should be daily
 PIR_THRESHOLD = 0.5   # Placeholder threshold for PIR sensor (if we were using it as a light sensor)
 STEP_DELAY = 0.002     # Speed of the motor (lower is faster). Each step will take 2 x 0.002 seconds.
 
@@ -20,26 +20,27 @@ STEP_DELAY = 0.002     # Speed of the motor (lower is faster). Each step will ta
 i2c = busio.I2C(board.GP17, board.GP16)
 sensor = adafruit_am2320.AM2320(i2c)
 
+
+# 2. TMC2209 Stepper Driver Setup
+step_pin = digitalio.DigitalInOut(board.GP5) # should change
+step_pin.direction = digitalio.Direction.OUTPUT
+
+dir_pin = digitalio.DigitalInOut(board.GP6) # should change
+dir_pin.direction = digitalio.Direction.OUTPUT
+
+enable_pin = digitalio.DigitalInOut(board.GP13) # should change
+enable_pin.direction = digitalio.Direction.OUTPUT
+enable_pin.value = False  # Set to False to ENABLE the TMC2209 driver
+
 '''
-# 2. PIR Sensor Setup (PRETENDING THIS IS A DIGITAL LIGHT SENSOR)
-pir = digitalio.DigitalInOut(board.D4) # should change
+# 3. PIR Sensor Setup (PRETENDING THIS IS A DIGITAL LIGHT SENSOR)
+pir = digitalio.DigitalInOut(board.GP4) # should change
 pir.direction = digitalio.Direction.INPUT
 
-# 3. Photoresistor Setup (Using your native analogio code)
+# 4. Photoresistor Setup (Using your native analogio code)
 photoresistor_pin = board.GP26_A0 # can change
 photoresistor = analogio.AnalogIn(photoresistor_pin)
 ADC_REF = photoresistor.reference_voltage
-
-# 4. TMC2209 Stepper Driver Setup
-step_pin = digitalio.DigitalInOut(board.D5) # should change
-step_pin.direction = digitalio.Direction.OUTPUT
-
-dir_pin = digitalio.DigitalInOut(board.D6) # should change
-dir_pin.direction = digitalio.Direction.OUTPUT
-
-enable_pin = digitalio.DigitalInOut(board.D13) # should change
-enable_pin.direction = digitalio.Direction.OUTPUT
-enable_pin.value = False  # Set to False to ENABLE the TMC2209 driver
 '''
 
 # ---------------- FUNCTIONS ---------------- #
@@ -83,17 +84,15 @@ while True:
         
         # Determine Mode based on our pretend light sensor
 
-        
-        
         # 2. Execute Logic Based on Modes
         if current_temp > HOT_TEMP_THRESHOLD and not is_hot_mode: 
                 print(f"Hot Mode Triggered: Temp: {current_temp}C. PIR Value: {pir_value}. Moving conveyor forward.")
-                #move_motor(steps=200, direction_forward=True) # 200 steps = 1 revolution for Nema 17
+                move_motor(steps=200, direction_forward=True) # 200 steps = 1 revolution for Nema 17
                 is_hot_mode = True
 
         elif current_temp < COLD_TEMP_THRESHOLD and is_hot_mode:
                 print(f"Cold Mode Triggered: Temp: {current_temp}C. PIR Value: {pir_value}. Moving conveyor backward.")
-                #move_motor(steps=200, direction_forward=False) # Move in reverse
+                move_motor(steps=200, direction_forward=False) # Move in reverse
                 is_hot_mode = False
 
         else:
